3508d2c61ea814e3ffe220514df1352ec6c8ddb5
JKusio <jakubkusiowski@gmail.com>
2022-07-03 00:04:04 +0200

    chore: move styles to src directory

---
+++ apps/frontend/src/pages/_app.tsx
@@ -0,0 +1,18 @@
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import '../styles/global.scss';
+
+function App({ Component, pageProps, router }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Pawnly</title>
+      </Head>
+      <div id="main" className="overflow-hidden">
+        <Component {...pageProps} />
+      </div>
+    </>
+  );
+}
+
+export default App;
