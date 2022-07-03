2ce0c107390064a1ff68e2f14bb105069663fc6c
JKusio <jakubkusiowski@gmail.com>
2022-07-03 21:39:17 +0200

    feat: add basic chess pieces

--- apps/frontend/src/components/ChessBoard/ChessBoard.tsx
+++ apps/frontend/src/components/ChessBoard/ChessBoard.tsx
@@ -1,6 +1,32 @@
+import { ChessBoardInstance } from './ChessBoardInstance';
+import { ChessPiece } from './ChessPiece';
+import { Color } from './Color';
+import { Piece } from './Piece';
+
 const ChessBoard = () => {
+  const chessBoardInstance = new ChessBoardInstance();
+
   return (
-    <div className="w-full h-full bg-[url('https://images.chesscomfiles.com/chess-themes/boards/walnut/150.jpg')] bg-center bg-cover"></div>
+    <div className="w-full h-full bg-[url('https://images.chesscomfiles.com/chess-themes/boards/walnut/150.jpg')] bg-center bg-cover relative">
+      {chessBoardInstance.board.map((row, y) =>
+        row.map((piece, x) => {
+          if (piece !== 0) {
+            return (
+              <ChessPiece
+                piece={Piece.Pawn}
+                color={Color.White}
+                position={{ x, y }}
+              />
+            );
+          }
+        })
+      )}
+      <ChessPiece
+        piece={Piece.Pawn}
+        color={Color.White}
+        position={{ x: 0, y: 0 }}
+      />
+    </div>
   );
 };
 
--- apps/frontend/src/components/ChessBoard/ChessPiece.tsx
+++ apps/frontend/src/components/ChessBoard/ChessPiece.tsx
@@ -1,7 +1,46 @@
-export const ChessPiece = () => {
+import Image from 'next/image';
+import { Color } from './Color';
+import { Piece } from './Piece';
+
+const PIECE_MAP = {
+  [Color.White]: {
+    [Piece.Pawn]: '/assets/chess/pieces/Chess_plt60.png',
+    [Piece.Knight]: '/assets/chess/pieces/Chess_nlt60.png',
+    [Piece.Bishop]: '/assets/chess/pieces/Chess_blt60.png',
+    [Piece.Rook]: '/assets/chess/pieces/Chess_rlt60.png',
+    [Piece.Queen]: '/assets/chess/pieces/Chess_qlt60.png',
+    [Piece.King]: '/assets/chess/pieces/Chess_klt60.png'
+  },
+  [Color.Black]: {
+    [Piece.Pawn]: '/assets/chess/pieces/Chess_pdt60.png',
+    [Piece.Knight]: '/assets/chess/pieces/Chess_ndt60.png',
+    [Piece.Bishop]: '/assets/chess/pieces/Chess_bdt60.png',
+    [Piece.Rook]: '/assets/chess/pieces/Chess_rdt60.png',
+    [Piece.Queen]: '/assets/chess/pieces/Chess_qdt60.png',
+    [Piece.King]: '/assets/chess/pieces/Chess_kdt60.png'
+  }
+};
+
+const mapPieceToImage = (piece: Piece, color: Color): string => {
+  return PIECE_MAP[color][piece];
+};
+
+export const ChessPiece = ({
+  piece,
+  color,
+  position
+}: {
+  piece: Piece;
+  color: Color;
+  position: { x: number; y: number };
+}) => {
+  const image = mapPieceToImage(piece, color);
+  const { x, y } = position;
+  const piecePositionClassName = `chess-piece-${y}${x}`;
+
   return (
-    <div>
-      <h1> I ma piece </h1>
+    <div className={`absolute w-1/8 h-1/8 ${piecePositionClassName}`}>
+      <Image src={image} layout="fill" />
     </div>
   );
 };
