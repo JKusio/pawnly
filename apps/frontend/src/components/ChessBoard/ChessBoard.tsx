bad50a482568b6551872e914089f4f7139d43af1
JKusio <jakubkusiowski@gmail.com>
2022-07-04 00:42:29 +0200

    feat: add draggable pieces

--- apps/frontend/src/components/ChessBoard/ChessBoard.tsx
+++ apps/frontend/src/components/ChessBoard/ChessBoard.tsx
@@ -14,6 +14,7 @@ const ChessBoardComponent = () => {
                 piece={piece.piece}
                 color={piece.color}
                 position={{ x, y }}
+                key={`piece-${y}-${x}`}
               />
             );
           }
--- apps/frontend/src/components/ChessBoard/ChessPiece.tsx
+++ apps/frontend/src/components/ChessBoard/ChessPiece.tsx
@@ -1,23 +1,29 @@
 import { Color } from 'lib/chess/pieces/Color';
 import { Piece } from 'lib/chess/pieces/Piece';
-import Image from 'next/image';
+import React, { useState } from 'react';
+import Draggable, {
+  DraggableData,
+  DraggableEvent,
+  DraggableEventHandler,
+  DraggableProps
+} from 'react-draggable';
 
 const PIECE_MAP = {
   [Color.White]: {
-    [Piece.Pawn]: '/assets/chess/pieces/Chess_plt60.png',
-    [Piece.Knight]: '/assets/chess/pieces/Chess_nlt60.png',
-    [Piece.Bishop]: '/assets/chess/pieces/Chess_blt60.png',
-    [Piece.Rook]: '/assets/chess/pieces/Chess_rlt60.png',
-    [Piece.Queen]: '/assets/chess/pieces/Chess_qlt60.png',
-    [Piece.King]: '/assets/chess/pieces/Chess_klt60.png'
+    [Piece.Pawn]: "bg-[url('/assets/chess/pieces/Chess_plt60.png')]",
+    [Piece.Knight]: "bg-[url('/assets/chess/pieces/Chess_nlt60.png')]",
+    [Piece.Bishop]: "bg-[url('/assets/chess/pieces/Chess_blt60.png')]",
+    [Piece.Rook]: "bg-[url('/assets/chess/pieces/Chess_rlt60.png')]",
+    [Piece.Queen]: "bg-[url('/assets/chess/pieces/Chess_qlt60.png')]",
+    [Piece.King]: "bg-[url('/assets/chess/pieces/Chess_klt60.png')]"
   },
   [Color.Black]: {
-    [Piece.Pawn]: '/assets/chess/pieces/Chess_pdt60.png',
-    [Piece.Knight]: '/assets/chess/pieces/Chess_ndt60.png',
-    [Piece.Bishop]: '/assets/chess/pieces/Chess_bdt60.png',
-    [Piece.Rook]: '/assets/chess/pieces/Chess_rdt60.png',
-    [Piece.Queen]: '/assets/chess/pieces/Chess_qdt60.png',
-    [Piece.King]: '/assets/chess/pieces/Chess_kdt60.png'
+    [Piece.Pawn]: "bg-[url('/assets/chess/pieces/Chess_pdt60.png')]",
+    [Piece.Knight]: "bg-[url('/assets/chess/pieces/Chess_ndt60.png')]",
+    [Piece.Bishop]: "bg-[url('/assets/chess/pieces/Chess_bdt60.png')]",
+    [Piece.Rook]: "bg-[url('/assets/chess/pieces/Chess_rdt60.png')]",
+    [Piece.Queen]: "bg-[url('/assets/chess/pieces/Chess_qdt60.png')]",
+    [Piece.King]: "bg-[url('/assets/chess/pieces/Chess_kdt60.png')]"
   }
 };
 
@@ -34,13 +40,35 @@ export const ChessPiece = ({
   color: Color;
   position: { x: number; y: number };
 }) => {
-  const image = mapPieceToImage(piece, color);
+  const background = mapPieceToImage(piece, color);
   const { x, y } = position;
   const piecePositionClassName = `chess-piece-${y}${x}`;
 
+  let [cursor, setCursor] = useState('cursor-grab');
+
+  const handleGrabStart = (
+    e: DraggableEvent,
+    data: DraggableData
+  ): void | false => {
+    setCursor('cursor-grabbing');
+  };
+
+  const handleGrabStop = (
+    e: DraggableEvent,
+    data: DraggableData
+  ): void | false => {
+    setCursor('cursor-grab');
+  };
+
   return (
-    <div className={`absolute w-1/8 h-1/8 ${piecePositionClassName}`}>
-      <Image src={image} layout="fill" />
-    </div>
+    <Draggable
+      onStart={handleGrabStart}
+      onStop={handleGrabStop}
+      bounds="parent"
+    >
+      <div
+        className={`absolute w-1/8 h-1/8 ${piecePositionClassName} ${background} bg-cover ${cursor} caret-transparent`}
+      ></div>
+    </Draggable>
   );
 };
